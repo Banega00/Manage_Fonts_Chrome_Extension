50b8b4112f627fd1c294e9cb72729d35a7353577
Banega00 <bane.stojanovic00@gmail.com>
2022-08-09 13:02:51 +0200

    toggle edit div

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 let selectedFont;
+let mode = 'no-edit' //no-edit, edit
+
+const editSvg = '<svg fill="#ffffff" style="margin-left:5px" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 30 30" width="16px" height="16px">    <path d="M 22.828125 3 C 22.316375 3 21.804562 3.1954375 21.414062 3.5859375 L 19 6 L 24 11 L 26.414062 8.5859375 C 27.195062 7.8049375 27.195062 6.5388125 26.414062 5.7578125 L 24.242188 3.5859375 C 23.851688 3.1954375 23.339875 3 22.828125 3 z M 17 8 L 5.2597656 19.740234 C 5.2597656 19.740234 6.1775313 19.658 6.5195312 20 C 6.8615312 20.342 6.58 22.58 7 23 C 7.42 23.42 9.6438906 23.124359 9.9628906 23.443359 C 10.281891 23.762359 10.259766 24.740234 10.259766 24.740234 L 22 13 L 17 8 z M 4 23 L 3.0566406 25.671875 A 1 1 0 0 0 3 26 A 1 1 0 0 0 4 27 A 1 1 0 0 0 4.328125 26.943359 A 1 1 0 0 0 4.3378906 26.939453 L 4.3632812 26.931641 A 1 1 0 0 0 4.3691406 26.927734 L 7 26 L 5.5 24.5 L 4 23 z"/></svg>'
 const inspectFont = () => {
     chrome.storage.sync.get(['fontForInspection'], function (result) {
         selectedFont = result.fontForInspection;
@@ -8,13 +11,40 @@ const inspectFont = () => {
             return;
         }
         const inspectFontContainer = document.querySelector('.font-inspect-container')
+
         inspectFontContainer.innerHTML =
-            `<div class="font-inspect-container">
-        <div>Font family: ${fontFamily}</div>
-        <div>Font size: ${fontSize} </div>
-        <div>Font weight: ${fontWeight}</div>
-        <div>Font color: ${color}</div>
-    </div>`
+            `
+        <div class="font-inspect-container">
+            <div class="no-edit-div">
+                <div>Font family: <b>${fontFamily}</b>  </div>
+                <div>Font size: <b>${fontSize} </b> </div>
+                <div>Font weight: <b>${fontWeight}</b> </div>
+                <div>Font color: <span class="color-box" style="background-color:${color}">${'&nbsp;'.repeat(10)}</span></div>
+            </div>
+
+            <div style="display:none;" class="edit-div">
+                EDIT DIV
+            </div>
+
+            <div class='edit-btn'>Edit ${editSvg} </div>
+        </div>`
+
+        setTimeout(() => {
+            const editBtn = document.querySelector('.font-inspect-container .edit-btn')
+            editBtn.addEventListener('click', (event) => {
+                if(mode == 'no-edit') mode = 'edit'
+                else if(mode == 'edit') mode = 'no-edit'
+
+                console.log(mode)
+                if(mode == 'edit'){
+                    document.querySelector('.no-edit-div').style.display='none'
+                    document.querySelector('.edit-div').style.display='block'
+                }else if(mode == 'no-edit'){
+                    document.querySelector('.no-edit-div').style.display='block'
+                    document.querySelector('.edit-div').style.display='none'
+                }
+            })
+        }, 0)
     });
 }
 
